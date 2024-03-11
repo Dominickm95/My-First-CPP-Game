17229bc4d5a09e454ea0292d6a0ebdff296fbb70
Dominickm95 <dominickmason718@gmail.com>
2024-03-10 17:34:56 -0700

    Cleaned up code and created collison functions

--- game.cpp
+++ game.cpp
@@ -8,6 +8,33 @@ float arena_half_size_x = 85, arena_half_size_y = 45;
 float player_half_size_x = 2.5, player_half_size_y = 12;
 float ball_p_x, ball_p_y, ball_dp_x = 100, ball_dp_y, ball_half_size = 1;
 
+internal void 
+simulate_player(float *p, float *dp, float ddp, float dt) {
+	ddp -= *dp * 10.f;
+
+	*p = *p + *dp * dt + ddp * dt * dt * .5f;
+	*dp = *dp + ddp * dt;
+
+	if (*p + player_half_size_y > arena_half_size_y) {
+		*p = arena_half_size_y - player_half_size_y;
+		*dp = 0;
+	}
+	else if (*p - player_half_size_y < -arena_half_size_y) {
+		*p = -arena_half_size_y + player_half_size_y;
+		*dp = 0;
+	}
+}
+
+internal bool
+aabb_vs_aabb(float p1x, float p1y, float hs1x, float hs1y,
+	float p2x, float p2y, float hs2x, float hs2y) {
+	return (p1x + hs1x > p2x - hs2x &&
+		p1x - hs1x < p2x + hs2x &&
+		p1y + hs1y > p2y - hs2y &&
+		p1y + hs1y < p2y + hs2y);
+}
+
+
 internal void
 simulate_game(Input* input, float dt) {
 	clear_screen(800080);
@@ -21,66 +48,49 @@ simulate_game(Input* input, float dt) {
 	if (is_down(BUTTON_W)) player_2ddp += 2000;
 	if (is_down(BUTTON_S)) player_2ddp -= 2000;
 	
-	//Player 1 Animation
-	player_1ddp -= player_1dp * 10.f;
+	simulate_player(&player_1p, &player_1dp, player_1ddp, dt);
+	simulate_player(&player_2p, &player_2dp, player_2ddp, dt);
 	
-	player_1p = player_1p + player_1dp * dt + player_1ddp * dt * dt * .5f;
-	player_1dp = player_1dp + player_1ddp * dt;
+	{
+		//Ball Physics 
+		ball_p_x += ball_dp_x * dt;
+		ball_p_y += ball_dp_y * dt;
 
-	if (player_1p + player_half_size_y > arena_half_size_y) {
-		player_1p = arena_half_size_y - player_half_size_y;
-		player_1p = 0;
-	}
-	else if (player_1p - player_half_size_y < -arena_half_size_y) {
-		player_1p = -arena_half_size_y + player_half_size_y;
-		player_1p = 0;
-	}
+		if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1p, player_half_size_x, player_half_size_y)) {
+			ball_p_x = 80 - player_half_size_x - ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_1p) * 2 + player_1dp * .75f;
+		}
+		else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2p, player_half_size_x, player_half_size_y)) {
+			ball_p_x = -80 + player_half_size_x + ball_half_size;
+			ball_dp_x *= -1;
+			ball_dp_y = (ball_p_y - player_2p) * 2 + player_2dp * .75f;
+		}
 
-	//Player 2 Animation 
-	player_2ddp -= player_2dp * 10.f;
+		if (ball_p_y + ball_half_size > arena_half_size_y) {
+			ball_p_y = arena_half_size_y - ball_half_size;
+			ball_dp_y *= -1;
+		}
+		else if (ball_p_y - ball_half_size < -arena_half_size_y) {
+			ball_p_y = -arena_half_size_y + ball_half_size;
+			ball_dp_y *= -1;
+		}
 
-	player_2p = player_2p + player_2dp * dt + player_2ddp * dt * dt * .5f;
-	player_2dp = player_2dp + player_2ddp * dt;
-
-	if (player_2p + player_half_size_y > arena_half_size_y) {
-		player_2p = arena_half_size_y - player_half_size_y;
-		player_2p = 0;
-	}
-	else if (player_2p - player_half_size_y < -arena_half_size_y) {
-		player_2p = -arena_half_size_y + player_half_size_y;
-		player_2p = 0;
-	}	
-
-	//Ball Physics 
-	ball_p_x += ball_dp_x * dt;
-	ball_p_y += ball_dp_y * dt;
-
-	
-	if (ball_p_x + ball_half_size > 80 - player_half_size_x &&
-		ball_p_x - ball_half_size < 80 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_1p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_1p + player_half_size_y) {
-		ball_p_x = 80 - player_half_size_x - ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_dp_y - player_1p)* 2 + player_1dp * .75f; 
-	} else if (ball_p_x + ball_half_size > -80 - player_half_size_x &&
-		ball_p_x - ball_half_size < -80 + player_half_size_x &&
-		ball_p_y + ball_half_size > player_2p - player_half_size_y &&
-		ball_p_y + ball_half_size < player_2p + player_half_size_y) {
-		ball_p_x = -80 + player_half_size_x + ball_half_size;
-		ball_dp_x *= -1;
-		ball_dp_y = (ball_dp_y - player_2p) * 2 + player_2dp * .75f;
+		if (ball_p_x + ball_half_size > arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
+		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+			ball_dp_x *= -1;
+			ball_dp_y = 0;
+			ball_p_x = 0;
+			ball_p_y = 0;
+		}
 	}
 
-	if (ball_p_y + ball_half_size > arena_half_size_y) {
-		ball_p_y = arena_half_size_y - ball_half_size;
-		ball_dp_y *= -1;
-	}
-	else if (ball_p_y - ball_half_size < -arena_half_size_y) {
-		ball_p_y = -arena_half_size_y + ball_half_size;
-		ball_dp_y *= -1;
-	}
-	
+
 	//Player and ball render 
 	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff);
 
